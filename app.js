0c89e00316dd5b593b30ddf6d435037e09436aa2
foreverxxxxxx <emineogutlu3@gmail.com>
2024-08-28 21:04:13 +0300

    Added product deletion endpoint; finds the product by ID, removes it from the list, and returns it.

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.post("/api/products", (req, res) => {
     const { error } =  validateProduct(req.body);
 
     if(error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
+      return   res.status(400).send(result.error.details[0].message);
+       
     }
 
     const product = {
@@ -42,14 +42,14 @@ app.post("/api/products", (req, res) => {
 app.put("/api/products/:id", (req, res) => {
     const product = products.find(p => p.id == req.params.id);
     if(!product) {
-        res.status(404).send("aradığınız ürün bulunamadı.");
+       return  res.status(404).send("aradığınız ürün bulunamadı.");
     }
 
     const { error } = validateProduct(req.body);
 
     if(error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
+     return   res.status(400).send(result.error.details[0].message);
+        
     }
 
     product.name = req.body.name;
@@ -58,11 +58,24 @@ app.put("/api/products/:id", (req, res) => {
     res.send(product);
 });
 
+app.delete("/api/products/:id", (req, res) => {
+    const product = products.find(p => p.id == req.params.id);
+
+    if(!product) {
+       return  res.status(404).send("aradığınız ürün bulunamadı.");
+    }
+
+    const index=products.indexOf(product);
+    products.splice(index,1);
+    res.send(product);
+  
+});
+
 app.get("/api/products/:id", (req, res) => {
     const product = products.find(p => p.id == req.params.id);
 
     if(!product) {
-        res.status(404).send("aradığınız ürün bulunamadı.");
+      return   res.status(404).send("aradığınız ürün bulunamadı.");
     }
     res.send(product);
 });
